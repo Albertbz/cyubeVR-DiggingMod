5a8eb119f141d51320768aff4c4784d61e904639
Albertbz <mail@albertbz.com>
2022-04-20 21:56:35 +0200

    Started work on using inheritance and polymorphism

--- ProjectFolder/ProjectFiles/Source/DiggingBlock.cpp
+++ ProjectFolder/ProjectFiles/Source/DiggingBlock.cpp
@@ -1,13 +1,11 @@
-#include "QuarryBlock.h"
+#include "DiggingBlock.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <algorithm>
 
-QuarryBlock::QuarryBlock(CoordinateInBlocks blockPosition) 
-	: DiggingBlock(blockPosition) {
-	/*
+DiggingBlock::DiggingBlock(CoordinateInBlocks blockPosition) {
 	this->size = 2;
 	this->depth = 5;
 	this->blockPosition = blockPosition;
@@ -15,27 +13,23 @@ QuarryBlock::QuarryBlock(CoordinateInBlocks blockPosition)
 	this->updateDigBlock();
 	this->currentDigBlock[2] = -1;
 	this->showNormal(true);
-	*/
 }
 
-QuarryBlock::QuarryBlock(int size, int depth, CoordinateInBlocks blockPosition, int currentMode, std::array<int, 3> currentDigBlock, std::array<int, 4> cornerBlockTypes) 
-	: DiggingBlock(size, depth, blockPosition, currentMode, currentDigBlock, cornerBlockTypes) {
-	/*
+DiggingBlock::DiggingBlock(int size, int depth, CoordinateInBlocks blockPosition, int currentMode, std::array<int, 3> currentDigBlock, std::array<int, 4> cornerBlockTypes) {
 	this->size = size;
 	this->depth = depth;
 	this->blockPosition = blockPosition;
 	this->currentMode = currentMode;
 	this->currentDigBlock = currentDigBlock;
 	this->cornerBlockTypes = cornerBlockTypes;
-	*/
 }
 
 
-CoordinateInBlocks QuarryBlock::getBlockPosition() {
+CoordinateInBlocks DiggingBlock::getBlockPosition() {
 	return this->blockPosition;
 }
 
-void QuarryBlock::addCorners() {
+void DiggingBlock::addCorners() {
 	cornerBlockTypes[0] = (uint8_t)GetBlock(blockPosition + CoordinateInBlocks(size, -size, 0)).Type;
 	cornerBlockTypes[1] = (uint8_t)GetBlock(blockPosition + CoordinateInBlocks(size, size, 0)).Type;
 	cornerBlockTypes[2] = (uint8_t)GetBlock(blockPosition + CoordinateInBlocks(-size, -size, 0)).Type;
@@ -46,14 +40,14 @@ void QuarryBlock::addCorners() {
 	SetBlock(blockPosition + CoordinateInBlocks(-size, size, 0), mark4BlockID);
 }
 
-void QuarryBlock::removeCorners() {
+void DiggingBlock::removeCorners() {
 	SetBlock(blockPosition + CoordinateInBlocks(size, -size, 0), (EBlockType)cornerBlockTypes[0]);
 	SetBlock(blockPosition + CoordinateInBlocks(size, size, 0), (EBlockType)cornerBlockTypes[1]);
 	SetBlock(blockPosition + CoordinateInBlocks(-size, -size, 0), (EBlockType)cornerBlockTypes[2]);
 	SetBlock(blockPosition + CoordinateInBlocks(-size, size, 0), (EBlockType)cornerBlockTypes[3]);
 }
 
-void QuarryBlock::incrementSize() {
+void DiggingBlock::incrementSize() {
 	if (currentMode == 2) {
 		removeCorners();
 		size++;
@@ -63,7 +57,7 @@ void QuarryBlock::incrementSize() {
 	}
 }
 
-void QuarryBlock::decrementSize() {
+void DiggingBlock::decrementSize() {
 	if (currentMode == 2 && size > 2) {
 		removeCorners();
 		size--;
@@ -73,27 +67,27 @@ void QuarryBlock::decrementSize() {
 	}
 }
 
-void QuarryBlock::incrementDepth() {
+void DiggingBlock::incrementDepth() {
 	if (currentMode == 2) {
 		depth++;
 		printDepth();
 	}
 }
 
-void QuarryBlock::decrementDepth() {
+void DiggingBlock::decrementDepth() {
 	if (currentMode == 2 && depth > 1) {
 		depth--;
 		printDepth();
 	}
 }
 
-void QuarryBlock::printDepth() {
+void DiggingBlock::printDepth() {
 	std::wstring message = L"Depth: ";
 	message.append(std::to_wstring(depth));
 	SpawnHintText(CoordinateInCentimeters(blockPosition) + CoordinateInCentimeters(0, 0, 125), message, 1, 1.2f);
 }
 
-void QuarryBlock::printSize() {
+void DiggingBlock::printSize() {
 	std::wstring message = L"Size: ";
 	int length = size * 2 + 1;
 	message.append(std::to_wstring(length));
@@ -102,7 +96,7 @@ void QuarryBlock::printSize() {
 	SpawnHintText(CoordinateInCentimeters(blockPosition) + CoordinateInCentimeters(0, 0, 125), message, 1, 1.2f);
 }
 
-void QuarryBlock::dig() {
+void DiggingBlock::dig() {
 	if (currentMode == 3) {
 		EBlockType currentBlockType = GetBlock(blockPosition + CoordinateInBlocks(currentDigBlock[0], currentDigBlock[1], currentDigBlock[2])).Type;
 		if (currentBlockType == EBlockType::Stone || currentBlockType == EBlockType::Dirt || currentBlockType == EBlockType::Grass) {
@@ -135,7 +129,7 @@ void QuarryBlock::dig() {
 	}
 }
 
-void QuarryBlock::toggleDigging() {
+void DiggingBlock::toggleDigging() {
 	if (currentMode == 1) {
 		showNormal(false);
 		showDigging(true);
@@ -148,7 +142,7 @@ void QuarryBlock::toggleDigging() {
 	}
 }
 
-void QuarryBlock::toggleSettings() {
+void DiggingBlock::toggleSettings() {
 	if (currentMode == 1) {
 		showNormal(false);
 		showSettings(true);
@@ -162,7 +156,7 @@ void QuarryBlock::toggleSettings() {
 	}
 }
 
-void QuarryBlock::showSettings(bool show) {
+void DiggingBlock::showSettings(bool show) {
 	if (show) {
 		addCorners();
 		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 4), setBlockID); // Set Settings block
@@ -175,13 +169,13 @@ void QuarryBlock::showSettings(bool show) {
 		removeCorners();
 		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 4), EBlockType::Air);
 		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 2), EBlockType::Air);
-		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 1), EBlockType::Air); 
-		SetBlock(blockPosition + CoordinateInBlocks(1, 0, 0), EBlockType::Air); 
+		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 1), EBlockType::Air);
+		SetBlock(blockPosition + CoordinateInBlocks(1, 0, 0), EBlockType::Air);
 		SetBlock(blockPosition + CoordinateInBlocks(-1, 0, 0), EBlockType::Air);
 	}
 }
 
-void QuarryBlock::showNormal(bool show) {
+void DiggingBlock::showNormal(bool show) {
 	if (show) {
 		addCorners();
 		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 4), setBlockID); // Set Settings block
@@ -194,7 +188,7 @@ void QuarryBlock::showNormal(bool show) {
 	}
 }
 
-void QuarryBlock::showFinished(bool show) {
+void DiggingBlock::showFinished(bool show) {
 	if (show) {
 		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 6), exclBlockID); // Set Exclamation Mark block
 	}
@@ -203,7 +197,7 @@ void QuarryBlock::showFinished(bool show) {
 	}
 }
 
-void QuarryBlock::showDigging(bool show) {
+void DiggingBlock::showDigging(bool show) {
 	if (show) {
 		SetBlock(blockPosition + CoordinateInBlocks(0, 0, 6), crossBlockID); // Set Cross Mark block
 	}
@@ -212,7 +206,7 @@ void QuarryBlock::showDigging(bool show) {
 	}
 }
 
-void QuarryBlock::destroy() {
+void DiggingBlock::destroy() {
 	switch (currentMode) {
 	case 1:
 		showNormal(false);
@@ -230,26 +224,26 @@ void QuarryBlock::destroy() {
 	currentMode = 0;
 }
 
-void QuarryBlock::updateDigBlock() {
+void DiggingBlock::updateDigBlock() {
 	currentDigBlock[0] = size;
 	currentDigBlock[1] = size;
 	currentDigBlock[2] = -1;
 }
 
-void QuarryBlock::writeQuarryBlocks(std::ostream& file, const std::vector<QuarryBlock*>& quarryBlocks) {
-	for (auto b : quarryBlocks) {
+void DiggingBlock::writeBlocks(std::ostream& file, const std::vector<DiggingBlock*>& diggingBlocks) {
+	for (auto b : diggingBlocks) {
 		file << b->size << ';' << b->depth << ';' << b->blockPosition.X << ';' << b->blockPosition.Y << ';' << b->blockPosition.Z << ';'
-			 << b->currentMode << ';' << b->currentDigBlock[0] << ';' << b->currentDigBlock[1] << ';' << b->currentDigBlock[2] << ';'
-			 << b->cornerBlockTypes[0] << ';' << b->cornerBlockTypes[1] << ';' << b->cornerBlockTypes[2] << ';' << b->cornerBlockTypes[3] << '\n';
+			<< b->currentMode << ';' << b->currentDigBlock[0] << ';' << b->currentDigBlock[1] << ';' << b->currentDigBlock[2] << ';'
+			<< b->cornerBlockTypes[0] << ';' << b->cornerBlockTypes[1] << ';' << b->cornerBlockTypes[2] << ';' << b->cornerBlockTypes[3] << '\n';
 	}
 }
 
-void QuarryBlock::writeQuarryBlocks(std::ostream&& file, const std::vector<QuarryBlock*>& quarryBlocks) {
-	QuarryBlock::writeQuarryBlocks(file, quarryBlocks);
+void DiggingBlock::writeBlocks(std::ostream&& file, const std::vector<DiggingBlock*>& diggingBlocks) {
+	DiggingBlock::writeBlocks(file, diggingBlocks);
 }
 
-auto QuarryBlock::readQuarryBlocks(std::istream& file)->std::vector<QuarryBlock*> {
-	std::vector<QuarryBlock*> quarryBlocks;
+auto DiggingBlock::readBlocks(std::istream& file)->std::vector<DiggingBlock*> {
+	std::vector<DiggingBlock*> diggingBlocks;
 	auto line = std::string{};
 
 	while (std::getline(file, line)) {
@@ -271,11 +265,11 @@ auto QuarryBlock::readQuarryBlocks(std::istream& file)->std::vector<QuarryBlock*
 
 		std::array<int, 3> currentDigBlock{};
 		pos = line.find_first_of(';', pos + 1);
-		currentDigBlock[0] = std::stoi(std::string{line, pos + 1});
+		currentDigBlock[0] = std::stoi(std::string{ line, pos + 1 });
 		pos = line.find_first_of(';', pos + 1);
-		currentDigBlock[1] = std::stoi(std::string{line, pos + 1});
+		currentDigBlock[1] = std::stoi(std::string{ line, pos + 1 });
 		pos = line.find_first_of(';', pos + 1);
-		currentDigBlock[2] = std::stoi(std::string{line, pos + 1});
+		currentDigBlock[2] = std::stoi(std::string{ line, pos + 1 });
 
 		std::array<int, 4> cornerBlockTypes{};
 		pos = line.find_first_of(';', pos + 1);
@@ -286,14 +280,14 @@ auto QuarryBlock::readQuarryBlocks(std::istream& file)->std::vector<QuarryBlock*
 		cornerBlockTypes[2] = std::stoi(std::string{ line, pos + 1 });
 		pos = line.find_first_of(';', pos + 1);
 		cornerBlockTypes[3] = std::stoi(std::string{ line, pos + 1 });
-		
-		
-		quarryBlocks.push_back(new QuarryBlock{ size, depth, blockPosition, currentMode, currentDigBlock, cornerBlockTypes });
+
+
+		diggingBlocks.push_back(new DiggingBlock{ size, depth, blockPosition, currentMode, currentDigBlock, cornerBlockTypes });
 	}
 
-	return quarryBlocks;
+	return diggingBlocks;
 }
 
-auto QuarryBlock::readQuarryBlocks(std::istream&& file) -> std::vector<QuarryBlock*> {
-	return readQuarryBlocks(file);
+auto DiggingBlock::readBlocks(std::istream&& file) -> std::vector<DiggingBlock*> {
+	return readBlocks(file);
 }
--- ProjectFolder/ProjectFiles/Source/DiggingBlock.h
+++ ProjectFolder/ProjectFiles/Source/DiggingBlock.h
@@ -1,61 +1,55 @@
 #pragma once
 #include "GameAPI.h"
-#include "DiggingBlock.h"
 #include <array>
-class QuarryBlock : public DiggingBlock {
-/*
-private:
+
+class DiggingBlock {
+protected:
 	int size;									// Used to define the size of the area (area is size*2+1 x size*2+1).
 	int depth;									// How deep the hole will be.
 	CoordinateInBlocks blockPosition;			// The position of the block.
 	int currentMode;							// The current mode the Quarry block is in. 0 = destroyed, 1 = normal, 2 = settings, 3 = digging, 4 = finished digging.
 	std::array<int, 3> currentDigBlock;			// The current block that is being dug out, relative to blockPosition.
 	std::array<int, 4> cornerBlockTypes;		// The types of the four cornor blocks before they were changed.
-	enum {mark1BlockID = 430459851,				// Enum with all of the blocks used for methods.
-		  mark2BlockID = 430459852,
-		  mark3BlockID = 430459853,
-		  mark4BlockID = 430459854,
-		  setBlockID = 1473066958,
-		  upBlockID = 1473066956,
-		  downBlockID = 1473066955,
-		  inBlockID = 1473066957,
-		  outBlockID = 1473066954,
-		  checkBlockID = 1473066960,
-		  exclBlockID = 1473066959,
-		  crossBlockID = 1473066961
+	enum {
+		mark1BlockID = 430459851,				// Enum with all of the blocks used for methods.
+		mark2BlockID = 430459852,
+		mark3BlockID = 430459853,
+		mark4BlockID = 430459854,
+		setBlockID = 1473066958,
+		upBlockID = 1473066956,
+		downBlockID = 1473066955,
+		inBlockID = 1473066957,
+		outBlockID = 1473066954,
+		checkBlockID = 1473066960,
+		exclBlockID = 1473066959,
+		crossBlockID = 1473066961
 	};
-	*/
-	
 public:
-	// Default constructor
-	QuarryBlock();
-
 	/**
-	* Constructor for QuarryBlock used when a new Quarry
-	* block is placed down.
+	* Constructor for DiggingBlock for when a new block is
+	* placed down.
 	* 
-	* @param blockPosition The position of the Quarry block.
+	* @param blockPosition The position of the block.
 	*/
-	QuarryBlock(CoordinateInBlocks blockPosition);
+	DiggingBlock(CoordinateInBlocks blockPosition);
 
 	/**
-	* Constructor for QuarryBlock used when loading in 
-	* the file containing all currently placed Quarry
-	* blocks.
+	* Constructor for DiggingBlock for when loading in 
+	* the file containing all currently placed DiggingBlocks.
 	* 
 	* @param size The size of the area to be dug out.
 	* @param depth How deep the hole will be.
-	* @param blockPosition The position of the Quarry block.
-	* @param currentMode The current mode of the Quarry block.
+	* @param blockPosition The position of the DiggingBlock.
+	* @param currentMode The current mode of the DiggingBlock.
 	* @param currentDigBlock The coordinates for the block currently being dug out.
 	* @param cornerBlockTypes The types of the corner blocks.
 	*/
-	QuarryBlock(int size, int depth, CoordinateInBlocks blockPosition, int currentMode, std::array<int, 3> currentDigBlock, std::array<int, 4> cornerBlockTypes);
-
+	DiggingBlock(int size, int depth, CoordinateInBlocks blockPosition, int currentMode, std::array<int, 3> currentDigBlock, std::array<int, 4> cornerBlockTypes);
+	
 	/**
 	* Accessor method for the blockPosition field.
 	* 
-	* @return The position of the Quarry block.
+	* @return The position of the DiggingBlock.
 	*/
 	CoordinateInBlocks getBlockPosition();
 
@@ -83,7 +77,7 @@ public:
 	// Spawns a Text with the current size as the corresponding area.
 	void printSize();
 
-	// Digs/mines a single block if the Quarry block is digging at that moment.
+	// Digs/mines a single block if the DiggingBlock is digging at that moment.
 	void dig();
 
 	// Toggles digging mode.
@@ -94,14 +88,14 @@ public:
 
 	/**
 	* Sets or removes all of the blocks needed for the settings mode.
-	* 
+	*
 	* @param show Whether to show the blocks.
-  	*/
+	*/
 	void showSettings(bool show);
 
 	/**
 	* Sets or removes all of the blocks needed for the normal mode.
-	* 
+	*
 	* @param show Whether to show the blocks.
 	*/
 	void showNormal(bool show);
@@ -127,41 +121,41 @@ public:
 	void updateDigBlock();
 
 	/**
-	* Writes a vector/list of QuarryBlock instances to a file.
-	* 
-	* @param file The file to write all of the Quarry blocks to.
-	* @param quarryBlocks The vector containing all of the Quarry
-	*						  blocks currently in the world.
-	*/
-	static void writeQuarryBlocks(std::ostream& file, const std::vector<QuarryBlock*>& quarryBlocks);
-
-	/**
-	* Writes a vector/list of QuarryBlocks instances to a file.
+	* Writes a vector/list of DiggingBlock instances to a file.
 	*
-	* @param file The file to write all of the Quarry blocks to.
-	* @param quarryBlocks The vector containing all of the Quarry
-	*						  blocks currently in the world.
+	* @param file The file to write all of the instances to.
+	* @param diggingBlocks The vector containing all of the
+	*					   blocks currently in the world.
 	*/
-	static void writeQuarryBlocks(std::ostream&& file, const std::vector<QuarryBlock*>& quarryBlocks);
+	static void writeBlocks(std::ostream& file, const std::vector<DiggingBlock*>& diggingBlocks);
+
+	/**
+	* Writes a vector/list of DiggingBlock instances to a file.
+	*
+	* @param file The file to write all of the instances to.
+	* @param quarryBlocks The vector containing all of the
+	*					  blocks currently in the world.
+	*/
+	static void writeBlocks(std::ostream&& file, const std::vector<DiggingBlock*>& diggingBlocks);
 
 
 	/**
-	* Reads a file of instances of QuarryBlock and returns it as a vector.
-	* 
-	* @param file The file to read.
-	* 
-	* @return A vector containing all of the Quarry blocks currently
-	*		   in the world.
-	*/
-	static auto readQuarryBlocks(std::istream& file)->std::vector<QuarryBlock*>;
-
-	/**
-	* Reads a file of instances of QuarryBlock and returns it as a vector.
+	* Reads a file of instances of DiggingBlock and returns it as a vector.
 	*
 	* @param file The file to read.
 	*
-	* @return A vector containing all of the Quarry blocks currently
-	*		   in the world.
+	* @return A vector containing instances of all of the blocks currently
+	*		  in the world.
 	*/
-	static auto readQuarryBlocks(std::istream&& file)->std::vector<QuarryBlock*>;
+	static auto readBlocks(std::istream& file)->std::vector<DiggingBlock*>;
+
+	/**
+	* Reads a file of instances of DiggingBlock and returns it as a vector.
+	*
+	* @param file The file to read.
+	*
+	* @return A vector containing instances of all of the blocks currently
+	*		  in the world.
+	*/
+	static auto readBlocks(std::istream&& file)->std::vector<DiggingBlock*>;
 };
